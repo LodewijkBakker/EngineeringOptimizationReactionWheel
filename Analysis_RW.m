a8bef0888322e400076cad49d413b2bb7c2ff0e6
LodewijkBakker <75375396+LodewijkBakker@users.noreply.github.com>
2026-04-14 16:37:57 +0200

    Added dimension constraint to analysis

--- Analysis_RW.m
+++ Analysis_RW.m
@@ -1,4 +1,20 @@
 
+% --- Base Input Parameters ---
+D = 2;              % 2m Diameter [m] (Note: 2m is huge, stresses will be high!)
+t_ring = 0.2;      % 20cm ring thickness [m]
+W = 0.08;           % 8cm out-of-plane thickness [m]
+n = 10;              % 6 spokes
+k = 1000;            % Number of points to check
+b1 = 0.1;
+b2 = 0.1;
 
+%-- fixed parameters
+rho = 7850;         % Steel density [kg/m3]
+T = 1000;           % 1000 Nm Torque
 
-draw_reaction_wheel(0.02, 0.05, 0.3, 0.01, 0.02, 0.02, 4);
+% dynamic should be based on thickness spokes and such and such.
+r_hub = 0.5;        % 50cm hub radius [m]
+
+[correct_geometry, L_spoke] = dimension_constraints(t_ring, D, b1, b2, W, n);
+r_hub = D - L_spoke - t_ring;
+draw_reaction_wheel(t_ring, r_hub, D, b1, b2, W, n);
